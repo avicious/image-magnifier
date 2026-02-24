c2cc13713135fdeb7e03c894810c39b1d5fa1ff2
Shubham Kumar <shubham.kr2951@gmail.com>
2026-02-24 20:10:05 +0530

    app refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import ImageMagnifier from "./components/imageMagnifier";
+import SAMPLE_IMAGE from "./assets/sample.jpg";
 
 const App = () => {
   return (
     <div className="container">
       <ImageMagnifier
-        imageUrl="/src/assets/sample.jpg"
+        imageUrl={SAMPLE_IMAGE}
         magnifierHeight={200}
         magnifierWidth={200}
         zoomLevel={1.5}
